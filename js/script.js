c7dcf729a657b62344abb1be449bece5f37e7b2d
Dmitriy Khegay <dmitriy88k@gmail.com>
2023-06-18 10:12:35 -0400

    init commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+const firstInput = document.getElementById('digit_1');
+const secondInput = document.getElementById('digit_2');
+let result = document.getElementById('result');
+
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  
+
+  
+})
+
+
+
+
+
