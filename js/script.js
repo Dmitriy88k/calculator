e4bb704c890b748569637fa637d92042c87470cf
Dmitriy Khegay <dmitriy88k@gmail.com>
2023-06-19 01:28:32 -0400

    fixed input problems

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
-const form = document.getElementById('form');
-const firstInput = document.getElementById('digit_1');
-const secondInput = document.getElementById('digit_2');
-let result = document.getElementById('result');
+//Ivoke Ids from index.html
+const firstInput = document.getElementById('digit_1')
+const secondInput = document.getElementById('digit_2')
+const addSign = document.getElementById('addition')
+const subSign = document.getElementById('subtraction')
+const multSign = document.getElementById('multiplication');
+let totalValue = document.getElementById('result')
 
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+
+addSign.addEventListener('click', function() {
+  
+  //Получить значения с input
+  const usersNumber1 = Number(firstInput.value);
+  const usersNumber2 = Number(secondInput.value);
+
+  if (usersNumber1 == "" | usersNumber2 == "") {
+    alert("Input 1 or Input 2 is empty");
+  }
 
   
 
-  
+  //При нажатии на кнопку + получаем ответ
+  totalValue = usersNumber1 + usersNumber2;
+
+  //отобразить результат в <p> 
+  result.innerHTML = totalValue;
 })
+
+
+function subFunction () {
+  const usersNumber1 = Number(firstInput.value);
+  const usersNumber2 = Number(secondInput.value);
+
+  totalValue = usersNumber1 - usersNumber2;
+  result.innerHTML = totalValue;
+}  
+
+
+
+
+
 
 
 
