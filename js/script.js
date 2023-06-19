2149692b4d8bf33ce8e7392990f2f5ce2feaa5f6
Dmitriy Khegay <dmitriy88k@gmail.com>
2023-06-19 01:57:29 -0400

    if input is empty shows nothing in result field

--- js/script.js
+++ js/script.js
@@ -7,24 +7,28 @@ const multSign = document.getElementById('multiplication');
 let totalValue = document.getElementById('result')
 
 
-
 addSign.addEventListener('click', function() {
-  
+
   //Получить значения с input
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
+  //Если один из инпутов пустой, то выдает alert
   if (usersNumber1 == "" | usersNumber2 == "") {
-    alert("Input 1 or Input 2 is empty");
+    alert("Input 1 OR Input 2 is empty");
   }
-
-  
 
   //При нажатии на кнопку + получаем ответ
   totalValue = usersNumber1 + usersNumber2;
 
   //отобразить результат в <p> 
   result.innerHTML = totalValue;
+
+  if (usersNumber1 == "" | usersNumber2 =="") {
+    result.innerHTML = null;
+  }
+
+
 })
 
 
