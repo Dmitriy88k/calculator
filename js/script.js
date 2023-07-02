eba094287b2df9e2b326040de65cf68b6cb9ff2b
Almat Ybray <a.ybray@sdventures.com>
2023-07-02 22:00:38 +0300

    fixed addSign logic

--- js/script.js
+++ js/script.js
@@ -1,42 +1,48 @@
 //Ivoke Ids from index.html
-const firstInput = document.getElementById('digit_1')
-const secondInput = document.getElementById('digit_2')
-const addSign = document.getElementById('addition')
-const subSign = document.getElementById('subtraction')
-const multSign = document.getElementById('multiplication');
-const divSign = document.getElementById('division')
-const powSign = document.getElementById('power')
-const clearSign = document.getElementById('clear')
-let totalValue = document.getElementById('result')
+const firstInput = document.getElementById("digit_1");
+const secondInput = document.getElementById("digit_2");
+const addSign = document.getElementById("addition");
+const subSign = document.getElementById("subtraction");
+const multSign = document.getElementById("multiplication");
+const divSign = document.getElementById("division");
+const powSign = document.getElementById("power");
+const clearSign = document.getElementById("clear");
+const totalValue = document.getElementById("result");
+const firstInputErrorText = document.getElementById('input-error-1')
 
+// 1. Не показывать алерт, а отображать как ошибку под инпутом
+// 2. Изменить стили
+// 3. переписать остальные функции как addSign
 
+firstInput.addEventListener('input', (event) => {
+  const value = event.target.value
 
-addSign.addEventListener('click', function() {
+  if (!value) {
+    event.target.style.border = "1px solid red";
+    firstInputErrorText.style.display = 'inline';
+  } else {
+    firstInput.style.border = "none";
+    firstInputErrorText.style.display = 'none';
+  }
+})
+
+addSign.addEventListener("click", function () {
+  if (firstInput.value === "" || secondInput.value === "") {
+    return;
+  }
 
   //Получить значения с input
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
-
-  //Если один из инпутов пустой, то выдает alert
-  if (usersNumber1 == "" || usersNumber2 == "") {
-    alert("Input 1 OR Input 2 is empty");
-  }
-
   //При нажатии на кнопку + получаем ответ
-  totalValue = usersNumber1 + usersNumber2;
+  const sum = usersNumber1 + usersNumber2;
 
-  //отобразить результат в <p> 
-  result.innerHTML = totalValue;
+  //отобразить результат в <p>
+  totalValue.innerHTML = sum;
+});
 
-  if (usersNumber1 == "" || usersNumber2 =="") {
-    result.innerHTML = null;
-  }
-
-})
-
-
-subSign.addEventListener('click', function() {
+subSign.addEventListener("click", function () {
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
@@ -44,17 +50,14 @@ subSign.addEventListener('click', function() {
     alert("Input 1 OR Input 2 is empty");
   }
 
-  totalValue = usersNumber1 - usersNumber2;
+  totalValue.innerHTML = usersNumber1 - usersNumber2;
 
-  result.innerHTML = totalValue;
-  
-  if (usersNumber1 == "" || usersNumber2 =="") {
-    result.innerHTML = null;
+  if (usersNumber1 == "" || usersNumber2 == "") {
+    totalValue.innerHTML = null;
   }
+});
 
-})
-
-multSign.addEventListener('click', function() {
+multSign.addEventListener("click", function () {
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
@@ -62,17 +65,14 @@ multSign.addEventListener('click', function() {
     alert("Input 1 OR Input 2 is empty");
   }
 
-  totalValue = usersNumber1 * usersNumber2;
+  totalValue.innerHTML = usersNumber1 * usersNumber2;
 
-  result.innerHTML = totalValue;
-  
-  if (usersNumber1 == "" || usersNumber2 =="") {
-    result.innerHTML = null;
+  if (usersNumber1 == "" || usersNumber2 == "") {
+    totalValue.innerHTML = null;
   }
+});
 
-})
-
-divSign.addEventListener('click', function() {
+divSign.addEventListener("click", function () {
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
@@ -80,18 +80,14 @@ divSign.addEventListener('click', function() {
     alert("Input 1 OR Input 2 is empty");
   }
 
-  totalValue = usersNumber1 / usersNumber2;
+  totalValue.innerHTML = usersNumber1 / usersNumber2;
 
-  result.innerHTML = totalValue;
-  
-  if (usersNumber1 == "" || usersNumber2 =="") {
-    result.innerHTML = null;
+  if (usersNumber1 == "" || usersNumber2 == "") {
+    totalValue.innerHTML = null;
   }
+});
 
-})
-
-
-powSign.addEventListener('click', function() {
+powSign.addEventListener("click", function () {
   const usersNumber1 = Number(firstInput.value);
   const usersNumber2 = Number(secondInput.value);
 
@@ -99,27 +95,15 @@ powSign.addEventListener('click', function() {
     alert("Input 1 OR Input 2 is empty");
   }
 
-  let totalValue = Math.pow(usersNumber1, usersNumber2);
+  totalValue.innerHTML = Math.pow(usersNumber1, usersNumber2)
 
-  result.innerHTML = totalValue;
-
-  if (usersNumber1 == "" || usersNumber2 =="") {
-    result.innerHTML = null;
+  if (usersNumber1 == "" || usersNumber2 == "") {
+    totalValue.innerHTML = null;
   }
+});
 
-})
-
-clearSign.addEventListener('click', function() {
+clearSign.addEventListener("click", function () {
   firstInput.value = "";
   secondInput.value = "";
   result.innerHTML = null;
-})
-
-
-
-
-
-
-
-
-
+});
